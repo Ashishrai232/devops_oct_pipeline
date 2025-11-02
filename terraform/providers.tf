1a03b3c511f49f2a1fef9dc678090b9831a607e8
Ashish rai <155087010+Ashishrai232@users.noreply.github.com>
2025-11-03 00:03:30 +0530

    .

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -6,15 +6,14 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name = "backend-rg"
+    resource_group_name = "bu-project-mid"
     # access_key           = "abcdefghijklmnopqrstuvwxyz0123456789..."  # Can also be set via `ARM_ACCESS_KEY` environment variable.
-    storage_account_name = "midsem26211"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "tfstate"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    storage_account_name = "bubackends"                                 # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "mid-sem"                                  # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "terraform.tfstate"                   # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
 provider "azurerm" {
-  subscription_id = "81636ff2-7700-48a9-8aa6-9270d1bc92ae"
   features {}
 }
